36f51b84af94279facdc1f2759311e72cc80a298
mathematicalninja <github@mathematics.ninja>
2025-06-16 01:27:50 +0800

    Added "whole file" input method

--- inputFile.py
+++ inputFile.py
@@ -1,3 +1,12 @@
+def inputFile():
+
+    with open("input.txt","r") as file:
+        r = file.read()
+
+    yield r
+    raise StopIteration
+
+
 def inputLines():
     idx = 0
     with open("input.txt","r") as file:
@@ -5,6 +14,7 @@ def inputLines():
     while idx < len(fileLines):
          yield fileLines[idx]
          idx+=1
+    pass
 
 def inputChars():
     idx = 0
@@ -13,13 +23,23 @@ def inputChars():
     while idx < len(fileChars):
          yield fileChars[idx]
          idx+=1
+    pass
+
+def inputFile_example(exStr:str):
+
+    yield exStr
+    raise StopIteration
+
+
 
 def inputChar_example(exStr:str):
     idx = 0
     while idx < len(exStr):
         yield exStr[idx]
         idx+=1
-
     pass
+
+
 def inputLines_example(exStr:str):
     yield exStr
+    pass
