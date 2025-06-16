5d103fd0cd12a63db4a9a24a58ea18be29ec20a4
mathematicalninja <github@mathematics.ninja>
2025-06-16 09:03:08 +0800

    aux files for large data

---
+++ src/utils/auxFile.py
@@ -0,0 +1,22 @@
+from os.path import join
+
+def makeAuxFile(fileName:str):
+    filePath = join("inputFiles",fileName)
+    with open(filePath, "x"):
+        return
+
+def clearAuxFile(fileName:str):
+    filePath = join("inputFiles",fileName)
+    with open(filePath, "w"):
+        return
+
+def readAuxFile(fileName:str):
+    filePath = join("inputFiles",fileName)
+    with open(filePath, "r") as file:
+        return file.read()
+
+def writeAuxFile(fileName:str, text:str):
+    filePath = join("inputFiles",fileName)
+    with open(filePath, "w") as file:
+        file.write(text)
+        return
