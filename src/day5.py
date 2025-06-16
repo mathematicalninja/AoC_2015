7ebe5d574eb596eaa5c0303e86f8c8edf41b6e57
mathematicalninja <github@mathematics.ninja>
2025-06-16 08:23:17 +0800

    Day 5 success

---
+++ src/day5.py
@@ -0,0 +1,67 @@
+from icecream import ic
+from typing import Generator, Any
+
+from inputFile import inputLines
+
+vowels = ["a","e","i","o","u"]
+def tripleVowel(line:str)->bool:
+    tally  = 0
+    for vowel in vowels:
+        tally= tally + line.count(vowel)
+    return tally>2
+
+def doubleLetter(line:str)->bool:
+    for index in range(len(line)-1):
+        if line[index] == line[index+ 1 ]:
+            return True
+    return False
+
+pairs = ["ab", "cd", "pq", "xy"]
+def naughtyPair(line:str)->bool:
+    for pair in pairs:
+        if(line.find(pair) != -1):
+            return True
+    return False
+
+def part1(lines: Generator[str, Any, None]):
+    total = 0
+    for line in lines:
+        if(
+            tripleVowel(line) and
+            doubleLetter(line) and
+            not naughtyPair(line)
+        ):
+            total = total + 1
+    return total
+    pass
+
+def doubleDouble(line:str):
+    for index in range(len(line)-2):
+        pair = line[index:index+2]
+        leftover = line[index+2:]
+        if (leftover.find(pair) != -1):
+            return True
+    return False
+
+def spaceRepeat(line:str):
+    for index in range(len(line)-2):
+        if(line[index] == line[index+2]):
+            return True
+    return False
+
+
+def part2(lines: Generator[str, Any, None]):
+    total = 0
+    for line in lines:
+        if(doubleDouble(line) and spaceRepeat(line)):
+            total = total + 1
+    return total
+    pass
+
+
+if __name__ == "__main__":
+    lines = inputLines(5)
+    print("part 1: ", part1(lines))
+    lines2 = inputLines(5)
+    print("part 2: ", part2(lines2))
+    pass
---
+++ src/tests/day5_test.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+from typing import List, Tuple
+from os.path import isfile, join
+
+from src.inputFile import inputLines_example, inputLines
+
+from src.day5 import part1, part2
+
+testCases_part1:List[Tuple[str,int]]=[
+    ("ugknbfddgicrmopn",1),
+    ("jchzalrnumimnmhp",0),
+    ("haegwjzuvuyypxyu",0),
+    ("dvszwmarrgswjxmb",0),
+]
+
+testCases_part2:List[Tuple[str,int]]=[
+
+    ("qjhvhtzxzqqjkmpb",1),
+    ("xxyxx",1),
+    ("aaa",0),
+    ("uurcxstgmygtbstg",0),
+    ("ieodomkazucvgmuy",0)
+]
+
+class Tests(TestCase):
+    def test_part1(self):
+        for case in testCases_part1:
+            self.assertEqual(part1(inputLines_example(case[0])),case[1])
+            pass
+    def test_part2(self):
+        for case in testCases_part2:
+            self.assertEqual(part2(inputLines_example(case[0])),case[1])
+            pass
+
+
+    def test_answer1(self):
+        answerPath = join("answers", "day5.txt")
+        inputPath = join("inputFiles", "day5.txt")
+        if isfile(answerPath) and isfile(inputPath):
+            with open (answerPath) as A:
+                answer1, answer2 = A.readlines()
+            fullinput = inputLines(5)
+
+            self.assertEqual(part1(fullinput),int(answer1.replace("\\n","")))
+
+
+        else:
+            pass
+
+    def test_answer2(self):
+        answerPath = join("answers", "day5.txt")
+        inputPath = join("inputFiles", "day5.txt")
+        if isfile(answerPath) and isfile(inputPath):
+            with open (answerPath) as A:
+                answer1, answer2 = A.readlines()
+            fullinput = inputLines(5)
+
+            self.assertEqual(part2(fullinput),int(answer2.replace("\\n","")))
+
+
+        else:
+            pass
