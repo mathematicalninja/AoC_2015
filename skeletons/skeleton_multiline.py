061f25fba38fa51606302206ff3ff6e258be8217
mathematicalninja <github@mathematics.ninja>
2025-06-23 10:06:19 +0800

    multiline skeleton

---
+++ skeletons/skeleton_multiline.py
@@ -0,0 +1,22 @@
+from icecream import ic
+from typing import Generator, Any
+
+from inputFile import inputLines
+
+def part1(lines: Generator[str, Any, None]):
+    for line in lines:
+        pass
+    pass
+
+def part2(lines: Generator[str, Any, None]):
+    for line in lines:
+        pass
+    pass
+
+
+if __name__ == "__main__":
+    # lines = inputLines(DAYNUMBER)
+    # print("part 1: ", part1(lines))
+    # lines2 = inputLines(DAYNUMBER)
+    # print("part 2: ", part2(lines2))
+    pass
