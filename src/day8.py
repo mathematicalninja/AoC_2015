74fcae9a33aa2c43347c37ce9235c6020ea4502a
mathematicalninja <github@mathematics.ninja>
2025-07-06 15:49:46 +0800

    double slash

---
+++ src/day8.py
@@ -0,0 +1,42 @@
+from icecream import ic
+from typing import Generator, Any, Tuple
+
+from inputFile import inputLines
+
+# https://adventofcode.com/2015/day/8
+
+
+def doubleSlash(line)->Tuple[int,str]:
+    count = 0
+    # print("\\\\")
+    while True:
+        idx = line.find("\\\\")
+        if(idx==-1):
+            break
+        count += 1
+        left = line[:idx]
+        right = line[idx+2:]
+        line = left+right
+    return count, line
+
+
+
+
+def part1(lines: Generator[str, Any, None]):
+    for line in lines:
+        pass
+    pass
+
+
+def part2(lines: Generator[str, Any, None]):
+    for line in lines:
+        pass
+    pass
+
+
+if __name__ == "__main__":
+    lines = inputLines(8)
+    print("part 1: ", part1(lines))
+    lines2 = inputLines(8)
+    print("part 2: ", part2(lines2))
+    pass
---
+++ src/tests/day8_test.py
@@ -0,0 +1,70 @@
+from unittest import TestCase
+from typing import List, Tuple
+from os.path import isfile, join
+
+from src.inputFile import inputLines_example, inputLines
+
+from src.day8 import part1, part2, doubleSlash
+
+testCases_part1:List[Tuple[str,int,int]]=[
+("",2, 0),
+("abc",5,3),
+("aaa\"aaa",10,7),
+("\x27",6,1)
+]
+
+testCases_part2:List[Tuple[str,int]]=[
+
+]
+
+
+class Functions(TestCase):
+    # def setUp(self) -> None:
+    #     pass
+
+    # def tearDown(self) -> None:
+    #     pass
+
+    def test_doubleSlash(self):
+        self.assertEqual(doubleSlash("aa\\\\bb")[0], 1)
+        self.assertEqual(doubleSlash("aa\\\\bb")[1], "aabb")
+
+class Parts(TestCase):
+    def test_part1(self):
+        for case in testCases_part1:
+            self.assertEqual(part1(inputLines_example(case[0])),case[1])
+            pass
+    def test_part2(self):
+        for case in testCases_part2:
+            self.assertEqual(part2(inputLines_example(case[0])),case[1])
+            pass
+
+
+class Answers(TestCase):
+    def test_answer1(self):
+        answerPath = join("answers", "day8.txt")
+        inputPath = join("inputFiles", "day8.txt")
+        if isfile(answerPath) and isfile(inputPath):
+            with open (answerPath) as A:
+                answer1, answer2 = A.readlines()
+            fullinput = inputLines(8)
+
+            self.assertEqual(part1(fullinput),int(answer1.replace("\\n","")))
+
+
+        else:
+            pass
+
+    def test_answer2(self):
+        answerPath = join("answers", "day8.txt")
+        inputPath = join("inputFiles", "day8.txt")
+        if isfile(answerPath) and isfile(inputPath):
+            with open (answerPath) as A:
+                answer1, answer2 = A.readlines()
+            fullinput = inputLines(8)
+
+            self.assertEqual(part2(fullinput),int(answer2.replace("\\n","")))
+
+
+        else:
+            pass
