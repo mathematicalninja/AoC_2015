7dcd9eb1df5e4647757ea1b6ef0354a2b3db9414
mathematicalninja <github@mathematics.ninja>
2025-07-06 19:49:11 +0800

    Outline

--- src/day9.py
+++ src/day9.py
@@ -22,7 +22,20 @@ def part1(lines: Generator[str, Any, None]):
     # Note that the brute force method will calculate all possible paths which gives n!, with reversible directions this is n!/2.
     # In this case 8!/2 = 20160
     # This is doable
+
+    # Note: to remove half the paths we can just say that the first city in the list if Always visited before the last city.
+    # Blah blah combinatorics...trivial.
+
+
+    cities = set({})
+
+    singleSteps:dict[str,dict[str,int]] = {} # {start:{end: distance}}
+
     for line in lines:
+        CityA, CityB = getCities(line)
+        distance = getDistance(line)
+        cities.add(CityA)
+        cities.add(CityB)
         pass
     pass
 
